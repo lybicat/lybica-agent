9d183b0e0730d48f0d304953228df1bb8966791d
Zhang Yu <yu.3.zhang@nokia.com>
2015-12-08 16:38:28 +0800

    minor fix

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var os = require('os');
 var fs = require('fs');
 var config = require('./config');
 var spawn = require('child_process').spawn;
-var moment = require('moment');
 
 // TODO: extra requirements:
 // 1. lybica module should be in PYTHONPATH
@@ -49,7 +48,7 @@ Agent.prototype.run = function(task, callback) {
   fs.mkdir(workspace, function(err) {
     if (err) return callback(err);
 
-    var consoleStream = fs.createWriteStream(workspace + '/console_' + moment().format('YYYYMMDDHHmmss') + '.txt');
+    var consoleStream = fs.createWriteStream(workspace + '/' + task._id + '_console.txt');
 
     var runner = spawn('python', ['-m', 'lybica']);
 
@@ -100,6 +99,6 @@ socket.on('console', function(msg) {
 });
 
 socket.on('disconnect', function() {
-  console.log('agent disconnected!');
+  console.log('agent disconnected by remote server!');
 });
 
